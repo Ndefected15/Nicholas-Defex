14f5ef0a6fada81cc4c501435990320019e36fd6
Nick D <Ndefected15@gmail.com>
2022-07-05 21:55:45 -0400

    Added sticky header, Centered content, added email, resized photo and added skills

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,13 +4,12 @@ import coverImage from '../../assets/cover/cover-image.jpg';
 function About() {
 	return (
 		<section className="my-5">
-			<h1 id="about">Who is Nicholas Defex</h1>
-			<img
-				src={coverImage}
-				className="my-2"
-				style={{ width: '100%' }}
-				alt="cover"
-			/>
+			<h1 id="about" className="centered">
+				Who is Nicholas Defex
+			</h1>
+			<div className="centered">
+				<img src={coverImage} className="my-2" alt="cover" />
+			</div>
 			<div className="my-2">
 				<p>
 					Solution-oriented Full Stack Web Developer known as a diligent
@@ -19,6 +18,14 @@ function About() {
 					displaying the release dates of Marvel films. Eager to become familiar
 					in the world of web development and to continue to grow from the work
 					to be done.
+				</p>
+				<br></br>
+				<br></br>
+				<br></br>
+				<h1 className="centered">Skills</h1>
+				<p className="centered">
+					HTML, CSS, Javascript, SQL, MongoDB, Express.js, REACT.js, Node.js,
+					ORM, MVC, PWA, OOP
 				</p>
 			</div>
 		</section>
